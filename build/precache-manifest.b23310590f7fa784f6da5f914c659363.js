7d8611751fc572b42ae28df94ff15cdca2fbe6ff
luyaxiang <luyaxiang@gagogroup.com>
2020-10-26 13:55:13 +0800

    feat: new release

---
+++ build/precache-manifest.b23310590f7fa784f6da5f914c659363.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8ecd6dfdf28462c51e41f9b64428fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25e510307339ae92f4ad",
+    "url": "/static/css/2.44309f2b.chunk.css"
+  },
+  {
+    "revision": "cf0b0e2b184b9a528036",
+    "url": "/static/css/main.e5e438f4.chunk.css"
+  },
+  {
+    "revision": "25e510307339ae92f4ad",
+    "url": "/static/js/2.48cf1098.chunk.js"
+  },
+  {
+    "revision": "ce53c4b16522e4a5817476f862996bac",
+    "url": "/static/js/2.48cf1098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf0b0e2b184b9a528036",
+    "url": "/static/js/main.8b51bfe8.chunk.js"
+  },
+  {
+    "revision": "de9d7b2868e288e79b25",
+    "url": "/static/js/runtime-main.bb9ee77d.js"
+  }
+]);
